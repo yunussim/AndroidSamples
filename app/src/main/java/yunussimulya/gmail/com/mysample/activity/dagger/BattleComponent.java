159a28c1a8b2457aec5938fa1f068e164e872331
yunussim <yunussimulya@gmail.com>
2018-11-16 16:16:31 +0700

    Update dagger

--- app/src/main/java/yunussimulya/gmail/com/mysample/activity/dagger/BattleComponent.java
+++ app/src/main/java/yunussimulya/gmail/com/mysample/activity/dagger/BattleComponent.java
@@ -1,9 +1,13 @@
 package yunussimulya.gmail.com.mysample.activity.dagger;
 
 import dagger.Component;
+import yunussimulya.gmail.com.mysample.activity.dagger.object.Boltons;
+import yunussimulya.gmail.com.mysample.activity.dagger.object.Starks;
 import yunussimulya.gmail.com.mysample.activity.dagger.object.War;
 
 @Component
 public interface BattleComponent {
     War getWar();
+    Boltons getBoltons();
+    Starks getStarks();
 }
